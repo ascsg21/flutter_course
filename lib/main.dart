73070ad17c05810400154795d0ff3259dc539dcc
ascsg21 <lovelyroom77@gmail.com>
2018-11-26 13:55:25 +0900

    11월 26일 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
-main() {
-
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  build(context) {
-    return MaterialApp();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/food.jpg'),
+              Text('Food Paradise')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
