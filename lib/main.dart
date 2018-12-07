d97829a7dc670653e9a76e07fd64cce59ba610d2
ascsg21 <lovelyroom77@gmail.com>
2018-12-07 11:04:41 +0900

    12월 7일-1 commit

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,8 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
           brightness: Brightness.light,
           primaryColor: Colors.deepOrange,
-          accentColor: Colors.deepPurple),
+          accentColor: Colors.deepPurple,
+          buttonColor: Colors.deepPurple),
       // home: AuthPage(),
       routes: {
         '/': (BuildContext context) => AuthPage(),
